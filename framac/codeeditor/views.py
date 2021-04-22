f3bc6ed3484d42955beb12421a1061ed549626bd
TheBlueshock <rafal1klimek@gmail.com>
2021-04-22 21:11:44 +0200

    chopped up index.html

--- framac/codeeditor/views.py
+++ framac/codeeditor/views.py
@@ -1,3 +1,7 @@
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+
+class CodeEditorView(View):
+    def get(self, request):
+        return render(request, 'codeeditor/index.html')
